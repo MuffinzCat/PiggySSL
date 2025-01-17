0af6fff7d161647908f1a8c8b63a240016a1084b
Merlijn Tuttel <MuffinzCat@outlook.com>
2025-01-18 00:45:12 +0100

    removed redundant install for OS package on workflow

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import SSL_retriever.SSL_retriever as SSL_retriever
 import default_config_generation.config as config
 
-if not os.path.exists('default_config_generation.ini'):
+if not os.path.exists('config.ini'):
     config.create_default_config()
 
 SSL_retriever.ssl_getter()
